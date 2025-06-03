1e52cf7ce9db8b177cd7f2842fa81276f6f215c3
thomasdonlon <thomasdonlonii@gmail.com>
2025-06-02 22:12:59 -0500

    I was wrong, the issue was actually that I wasn't splitting the inventory list before printing

--- utils.py
+++ utils.py
@@ -38,7 +38,7 @@ def get_item_name(item_id):
     """
     Returns the name of the item based on its ID.
     """
-    if item_id in item_descriptions.keys():
+    if item_id in item_descriptions:
         return item_descriptions[item_id]
     else:
         return "Unknown Item"
